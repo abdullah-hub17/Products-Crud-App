d163fd4a7d23b9fdebbc092d71b23099a00c8d5c
Muhammad Abdullah Arshad <abdullaharshed7@gmail.com>
2025-03-17 16:43:49 +0500

    First Commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-CONNECTION_STRING = "mongodb://localhost:27017/test";
+CONNECTION_STRING = process.env.MONGO_URI;
 
 const connectDB = async () => {
-    try {
-      await mongoose.connect(CONNECTION_STRING);
-      console.log('MongoDB connected successfully');
-    } catch (error) {
-      console.error('MongoDB connection failed:', error);
-      process.exit(1); // Exit process with failure
-    }
-  };
-  
-  module.exports = connectDB;
+  try {
+    await mongoose.connect(CONNECTION_STRING);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
